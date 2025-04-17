a29b6ebcd6273c489427129e851a78c2e5705042
biandopa <biandopa@frba.utn.edu.ar>
2025-04-17 12:36:38 -0300

    Add cpu endpoints

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/sisoputnfrba/tp-golang/cpu/internal"
+	"github.com/sisoputnfrba/tp-golang/utils/log"
+)
+
+func main() {
+	logger := log.BuildLogger()
+
+	handler := http.NewServeMux()
+
+	handler.HandleFunc("/instrucciones", internal.RecibirInstrucciones)
+	handler.HandleFunc("/procesos", internal.RecibirProcesos)
+	handler.HandleFunc("/interrupciones", internal.RecibirInterrupciones)
+
+	if err := http.ListenAndServe(":8080", handler); err != nil {
+		logger.Error("Error starting server", "err", err)
+		panic(err)
+	}
 }
---
+++ cpu/internal/instructions.go
@@ -0,0 +1,61 @@
+package internal
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func RecibirInstrucciones(w http.ResponseWriter, r *http.Request) {
+	// Leer el cuerpo de la solicitud
+	decoder := json.NewDecoder(r.Body)
+	paquete := map[string]interface{}{}
+
+	// Guarda el valor del body en la variable paquete
+	err := decoder.Decode(&paquete)
+	if err != nil {
+		log.Printf("error al decodificar mensaje: %s\n", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("error al decodificar mensaje"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("ok"))
+}
+
+func RecibirProcesos(w http.ResponseWriter, r *http.Request) {
+	// Leer el cuerpo de la solicitud
+	decoder := json.NewDecoder(r.Body)
+	paquete := map[string]interface{}{}
+
+	// Guarda el valor del body en la variable paquete
+	err := decoder.Decode(&paquete)
+	if err != nil {
+		log.Printf("error al decodificar mensaje: %s\n", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("error al decodificar mensaje"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("ok"))
+}
+
+func RecibirInterrupciones(w http.ResponseWriter, r *http.Request) {
+	// Leer el cuerpo de la solicitud
+	decoder := json.NewDecoder(r.Body)
+	paquete := map[string]interface{}{}
+
+	// Guarda el valor del body en la variable paquete
+	err := decoder.Decode(&paquete)
+	if err != nil {
+		log.Printf("error al decodificar mensaje: %s\n", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("error al decodificar mensaje"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("ok"))
+}
